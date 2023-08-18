4bb0f3a71f1eca55dbfa51767a0e47b2df2406db
sangukO <71215834+sangukO@users.noreply.github.com>
2023-08-18 13:34:34 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
-    numbers.sort(function(a,b){return b - a});
-    return numbers[0]*numbers[1];
+    var [a, b] = numbers.sort(function(a,b){return b - a});
+    return a * b;
 }
