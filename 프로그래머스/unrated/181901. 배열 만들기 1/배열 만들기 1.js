2e8b00462f5e29d911ab2285df3ca85809dee459
sangukO <71215834+sangukO@users.noreply.github.com>
2023-09-04 09:36:03 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,9 +1,7 @@
 function solution(n, k) {
     var answer = [];
-    for (i=1;i<=n;i++) {
-        if (i%k==0) {
-            answer.push(i);
-        }
+    for (i=k;i<=n;i+=k) {
+        answer.push(i);
     }
     return answer;
 }
