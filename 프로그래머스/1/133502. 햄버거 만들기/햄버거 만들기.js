0532e3db427b3e89d79b2e6990e53ab16cadc5c4
sangukO <woqls1972@gmail.com>
2024-06-24 13:54:02 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,11 @@
+function solution(ingredient) {
+    var answer = 0;
+    for (i=0;i<ingredient.length;i++) {
+        if (ingredient[i]===1 && ingredient[i+1]===2 && ingredient[i+2] === 3 && ingredient[i+3]===1) {
+            ingredient.splice(i, 4);
+            answer++;
+            i=i-4;
+        }
+    }
+    return answer;
+}
