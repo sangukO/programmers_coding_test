01a34dc08cf2b1c0363240504cb3faa878717043
sangukO <woqls1972@gmail.com>
2024-07-01 10:53:53 +0900

    [level 4] Title: 호텔 방 배정, Time: 197.90 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -2,7 +2,6 @@
 function solution(k, room_number) {
     var answer = []; // 결과 저장
     var next = new Map(); // 다음 가능한 방 번호
-    
     function find(x) { // 주어진 방 번호 x가 배정 가능한지 확인 후 이미 배정된 경우 다음 가능한 방 번호 찾음
         if(!next.has(x)) { // x번 방이 비어있는 방일 경우
             next.set(x, x+1); // x번 배정됐으니 x+1로 가라고 설정
@@ -12,13 +11,10 @@ function solution(k, room_number) {
         next.set(x, root); // 방 1의 다음 방 번호는 4로 연결, 순서대로 3>4, 2>4, 1>4로 연결
         return root; // 4번 방 반환
     }
-    
     for(i=0;i<room_number.length;i++) {
         var room = room_number[i];
         var assigned = find(room);
         answer.push(assigned);
     }
-    
-    
     return answer;
 }
