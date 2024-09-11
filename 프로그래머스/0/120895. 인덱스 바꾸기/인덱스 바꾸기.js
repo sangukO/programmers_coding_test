4344fb842ee565fe2de380b54c115deae0d12b74
sangukO <woqls1972@gmail.com>
2024-09-11 11:39:32 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+
+    let arr = my_string.split('');
+
+    let temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
+
+    return arr.join('');
+}
