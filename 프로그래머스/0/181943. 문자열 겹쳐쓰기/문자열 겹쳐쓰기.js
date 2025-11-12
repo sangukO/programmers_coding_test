5b1c0a991c9be1a0c81c5eacb1fbbe8c1e92c426
sangukO <woqls1972@gmail.com>
2025-11-13 00:36:30 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,11 @@
+function solution(my_string, overwrite_string, s) {
+    let answer = "";
+    for (let i=0;i<my_string.length;i++) {
+        if (i >= s && i-s < overwrite_string.length) {
+            answer+=overwrite_string[i-s];
+        } else {
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
